b4f04bac9e142bae8efa4300e490e70b5b06371d
nikjust <36077796+nikjust@users.noreply.github.com>
2024-03-09 19:02:44 +0500

    Rotations

--- Assets/scripts/AgnleRotator.cs
+++ Assets/scripts/AgnleRotator.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class AngleRotator : MonoBehaviour
 {
     // Start is called before the first frame update
     private float angle;
+    public bool canRotate = false;
+    
 
     void Start()
     {
@@ -13,10 +15,31 @@ public class NewBehaviourScript : MonoBehaviour
     }
 
     // Update is called once per frame
+    private float lastX;
+    private float lastY;
+
     void Update()
     {
+        if (!canRotate)
+        {
+            return;
+        }
+
+        if (angle != 0 && (lastX != transform.rotation.eulerAngles.x || lastY != transform.rotation.eulerAngles.y))
+        {
+            angle = 0;
+        }
+
         if (Input.GetKeyUp(KeyCode.H))
+        {
             angle += 90;
-            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            var x = transform.rotation.eulerAngles.x;
+            var y = transform.rotation.eulerAngles.y;
+            var z = transform.rotation.eulerAngles.z;
+            transform.rotation = Quaternion.Euler(new Vector3(x, y, z + angle));
+        }
+
+        lastX = transform.rotation.eulerAngles.x;
+        lastY = transform.rotation.eulerAngles.y;
     }
 }
