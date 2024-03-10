76f7694698ceaa7b576e830a719be50d3fb4a4d7
maestroTW <nayan10122007@gmail.com>
2024-03-10 18:13:12 +0500

    Summary (required)

--- Assets/scripts/Button.cs
+++ Assets/scripts/Button.cs
@@ -11,16 +11,6 @@ public class Button : Activable
     public Material active;
     public Material unactive;
     private bool _activated = false;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public override bool IsActive()
     {
