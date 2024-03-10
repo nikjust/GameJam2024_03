1665d259c42473157ea8292c0d45ef1db616dec3
nikjust <36077796+nikjust@users.noreply.github.com>
2024-03-10 18:51:15 +0500

    Revert "Revert "brbrbr""
    This reverts commit e67fe4d0cbd1b6e50ec162d7da6613156e49df91.

---
+++ Assets/scripts/KillTrigger.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
